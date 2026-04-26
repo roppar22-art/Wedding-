8f4cf3fcb79f5fe537d66556546534e793d53262
roppar22-art <roppar22@gmail.com>
2026-04-27 02:48:39 +0400

    Remove comment regarding CSS animation
    Removed comment about CSS animation for the 'open' class.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function openEnvelope() {
     const wrapper = document.getElementById('envelopeWrapper');
-    // Class 'open' ကို ထည့်လိုက်ရင် CSS က Animation တွေ စလုပ်ပါလိမ့်မယ်
     wrapper.classList.add('open');
 }
